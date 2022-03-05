fb8468893a1adf7c79f4111d72a55dba521a5f46
Tiago Covalesky <tcovalesky@gmail.com>
2022-03-05 15:33:39 -0300

    creates agents resource

---
+++ src/agents/agents.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { AgentsService } from './agents.service';
+import { CreateAgentDto } from './dto/create-agent.dto';
+import { UpdateAgentDto } from './dto/update-agent.dto';
+
+@Controller('public/agents')
+export class AgentsController {
+  constructor(private readonly agentsService: AgentsService) {}
+
+  @Post()
+  create(@Body() createAgentDto: CreateAgentDto) {
+    return this.agentsService.create(createAgentDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.agentsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.agentsService.findOne(+id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateAgentDto: UpdateAgentDto) {
+    return this.agentsService.update(+id, updateAgentDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.agentsService.remove(+id);
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AgentsModule } from './agents/agents.module';
 
 @Module({
-  imports: [],
+  imports: [AgentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
